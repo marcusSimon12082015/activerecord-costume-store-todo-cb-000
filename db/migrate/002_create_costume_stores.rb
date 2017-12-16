2eefda6cb6005a6f0517f1e6bbbeeca4fcc9461a
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-16 16:15:06 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,11 +4,11 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
     create_table :costume_stores do |cs|
       cs.string :name
       cs.string :location
-      cs.integer :inventory
+      cs.integer :custome_inventory
       cs.integer :num_of_employees
       cs.boolean :still_in_business
       cs.time :opening_time
       cs.time :closing_time
     end
   end
-end 
+end
